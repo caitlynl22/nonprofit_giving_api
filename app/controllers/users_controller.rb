1579f4d55022ab8ef1fdbb3bffb2daa14283f76a
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-04-06 11:58:26 -0400

    Add user specs, model, and controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+  before_filter :authenticate, only: [:show, :update, :destroy]
+
+  def sign_in
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      render json: {token: user.token, id: user.id}
+    else
+      head :unauthorized
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: {token: @user.token, id: @user.id}
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user, status: 200
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render json: @user, status: 200
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    head :no_content
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :image_url, :bio, :token)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  has_secure_password
+
+  before_create :generate_token
+
+  def generate_token
+    return if token.present?
+    begin
+      self.token = SecureRandom.uuid.gsub(/\-/, '')
+    end while self.class.exists?(token: token)
+  end
+
 end
